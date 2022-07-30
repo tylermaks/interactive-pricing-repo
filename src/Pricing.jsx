381ca436a3eabb019d91056041e4472a78ef8c69
Tyler Maksymiw <tylermcodes@gmail.com>
2022-07-30 09:35:24 -0400

    Initial commit

---
+++ src/Pricing.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./App.css"
+
+function Pricing(){
+    return(
+        <div className="pricing-container">
+
+        </div>
+    )
+}
+
+export default Pricing;
